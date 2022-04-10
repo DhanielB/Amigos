1e92b97a977b739bc07792d1beec7dfbbabf3637
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-04-09 23:00:50 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./src/**/*.{js, jsx, ts, tsx}", "./src/components/**/*.{js, jsx, ts, tsx}"],
+  purge: [ "./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}", ],
   theme: {
     extend: {},
   },
