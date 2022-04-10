9e0e7e63789fefb6d007bf63a098fc52fe012358
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-04-09 21:35:09 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='
+      flex
+      h-screen
+      w-screen
+      bg-gray-400
+      items-center
+      font-extrabold
+      justify-center
+      tracking-tight
+      text-white
+      dark:bg-gray-900
+    '>
+    <div className='
+      ml-10
+      mr-10'>
+      <form action='https://docs.google.com/forms/u/0/d/e/1FAIpQLScR7o0rHme7MkATScMl4De9jR940jDX46F15ftHLgM9o2JDXg/formResponse'>
+        <h1 className='
+          mr-2
+          text-2xl
+        '>Participe da Minha</h1>
+        <h1 className='
+          text-2xl
+          text-pink-500
+        '>lista de amigos</h1>
+
+        <input className='
+          pt-1
+          pb-1
+          pl-2
+          w-40
+          border
+          text-black
+          border-gray-500
+          rounded
+          dark:bg-gray-100
+          outline-none
+        ' name='entry.411041762' placeholder='Digite seu nome' required/>
+        <button className='
+          ml-3
+          pt-1
+          pb-1
+          pl-2
+          pr-2
+          bg-pink-500
+          focus:outline-none
+          focus:ring-2
+          focus:ring-offset-2
+          focus:ring-pink-550
+          focus:bg-pink-550
+          rounded
+        '>Enviar</button>
+      </form>
+      </div>
+    </div>
+  )
+}
