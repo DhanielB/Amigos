494121424c18f24b7a844c3552a531491f92c881
Dhaniel Brandão <82909711+DhanielB@users.noreply.github.com>
2022-04-09 22:25:27 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ["./src/pages/**/*.{js, jsx, ts, tsx"],
+  content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"],
+  purge: {
+    content: ["./src/pages/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"]
+    // These options are passed through directly to PurgeCSS
+  },
   theme: {
     extend: {},
   },
